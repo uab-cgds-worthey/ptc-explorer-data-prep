c03f145a063fe7ff3e1dbde9b173acb335270797
Samuel Bharti <sbharti@uab.edu>
2025-09-26 04:33:41 -0500

    Update init.R
    Create output directory by runtime date

--- src/init.R
+++ src/init.R
@@ -3,6 +3,9 @@ library(tidyr)
 library(dplyr)
 
 ##### Define paths ######
-output_dir = paste0(getwd(),"/out/9-25-25/")
+output_dir = paste0(getwd(),"/out/",
+                    format.Date(Sys.Date(), "%m-%d-%Y"),
+                    "/")
 
+if(!output_dir) dir.create(output_dir)
 ##### Load shared datasets #####
