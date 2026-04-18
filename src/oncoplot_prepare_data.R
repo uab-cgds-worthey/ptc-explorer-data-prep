00e01c5f479d975e177ee8337dd1a73b761b6164
Samuel Bharti <sbharti@uab.edu>
2026-04-18 02:27:27 -0500

    update variables for latest data version colnames

--- src/oncoplot_prepare_data.R
+++ src/oncoplot_prepare_data.R
@@ -3,8 +3,20 @@
 
 #### Prepare variant level data for oncoplot ####
 
+if ("Case ID" %in% colnames(ptc_df_onco) && !"Case_id" %in% colnames(ptc_df_onco)) {
+  colnames(ptc_df_onco)[colnames(ptc_df_onco) == "Case ID"] <- "Case_id"
+}
+
+if ("User Classification" %in% colnames(ptc_df_onco) && !"User_Classification" %in% colnames(ptc_df_onco)) {
+  colnames(ptc_df_onco)[colnames(ptc_df_onco) == "User Classification"] <- "User_Classification"
+}
+
+if ("Germline_Class" %in% colnames(ptc_df_onco) && !"User_Classification" %in% colnames(ptc_df_onco)) {
+  colnames(ptc_df_onco)[colnames(ptc_df_onco) == "Germline_Class"] <- "User_Classification"
+}
+
 ### Check duplication in rows
-ptc_df_onco[duplicated(ptc_df_onco[,c("Genes","Type","Participant_id","Phenotype")]),]
+ptc_df_onco[duplicated(ptc_df_onco[,c("Genes","Type","Case_id","Phenotype")]),]
 
 ### Duplicate the dataframe 
 ptc_df_rmVUS <- ptc_df_onco
@@ -12,46 +24,37 @@ ptc_df_rmVUS <- ptc_df_onco
 ### Preview column value counts
 table(ptc_df_rmVUS$Phenotype)
 table(ptc_df_rmVUS$Type)
-table(ptc_df_rmVUS$Germline_Class)
+table(ptc_df_rmVUS$User_Classification)
 
 ### Combine type,germline class, and phenotype to new column
 ### This is the unique row value in oncoplot
 ptc_df_rmVUS$alt <- paste(ptc_df_rmVUS$Type,
-                          ptc_df_rmVUS$Germline_Class,
+                          ptc_df_rmVUS$User_Classification,
                           ptc_df_rmVUS$Phenotype,
                           sep = ";")
 
 table(ptc_df_rmVUS$alt)
 
 ### Check duplication based on new id
-ptc_df_rmVUS[duplicated(ptc_df_rmVUS[,c("Genes","alt","Participant_id")]),]
+ptc_df_rmVUS[duplicated(ptc_df_rmVUS[,c("Genes","alt","Case_id")]),]
 
 dim(ptc_df_rmVUS)
 
 ### Subset dataframe for required columns and check if duplication persist 
-ptc_df_rmVUS_alt <- ptc_df_rmVUS[,c("Genes","Participant_id","alt")]
+ptc_df_rmVUS_alt <- ptc_df_rmVUS[,c("Genes","Case_id","alt")]
 
 
-ptc_df_rmVUS_alt[duplicated(ptc_df_rmVUS[,c("Genes","Participant_id")]),]
+ptc_df_rmVUS_alt[duplicated(ptc_df_rmVUS[,c("Genes","Case_id")]),]
 
 ptc_df_rmVUS_alt_wide <- pivot_wider(ptc_df_rmVUS_alt, 
-                                     names_from = Participant_id, 
+                                     names_from = Case_id, 
                                      values_from = alt,
                                      names_prefix = "P_",
-                                     values_fill = NA)
+                                     values_fill = NA,
+                                     values_fn = function(x) paste(unique(x), collapse = ";"))
 
 
-ptc_df_rmVUS_alt_wide <- apply(ptc_df_rmVUS_alt_wide, c(1, 2), function(x) {
-  if (is.list(x)) {
-    # Join list elements with ";"
-    paste(unlist(x), collapse = ";")
-  } else {
-    as.character(x)  # Just convert to character if not a list
-  }
-})
-
-
-unique(ptc_df_rmVUS$Participant_id)
+unique(ptc_df_rmVUS$Case_id)
 dim(ptc_df_rmVUS)
 
 ptc_df_onco_ready <- ptc_df_rmVUS_alt_wide
--- src/prepare_variant_data.R
+++ src/prepare_variant_data.R
@@ -1,22 +1,50 @@
 ####### Latest Variant Table from BOX #######
 ptc_df_main = readxl::read_excel(
-  "data/2025-04-01_Variants_tumor-normal_TableS3_rev2_4app.xlsx",
-  sheet = "TableS3_rev2")
+  "data/2026-04-18_Additional-file-1.xlsx",
+  sheet = "TableS3")
 
 dim(ptc_df_main)
 
 colnames(ptc_df_main)
-sel_cols_onco <- c("Participant_id",
+# Resolve key input columns to support the latest table naming
+normalize_colnames <- function(x) {
+  gsub("[^a-z0-9]", "", tolower(x))
+}
+
+input_colnames <- colnames(ptc_df_main)
+input_colnames_clean <- normalize_colnames(input_colnames)
+
+resolve_input_col <- function(normalized_candidates) {
+  match_idx <- which(input_colnames_clean %in% normalized_candidates)
+  if (length(match_idx) == 0) {
+    return(NA_character_)
+  }
+  input_colnames[match_idx[1]]
+}
+
+case_col <- resolve_input_col(c("caseid", "participantid"))
+germline_col <- resolve_input_col(c("userclassification", "germlineclass"))
+
+if (anyNA(c(case_col, germline_col))) {
+  stop(
+    "Required ID/classification columns were not found in variant input table. ",
+    "Expected one of: case_id/participant_id and user classification/germline class."
+  )
+}
+
+sel_cols_onco <- c(case_col,
                    "Phenotype",
                    "Type",
                    "Genes",
                    "Variant",
-                   "Germline_Class",
+                   germline_col,
                    "Allelic balance",
                    "Chromosome",
                    "Position")
 
 ptc_df_onco <- ptc_df_main[,sel_cols_onco]
+colnames(ptc_df_onco)[colnames(ptc_df_onco) == case_col] <- "Case_id"
+colnames(ptc_df_onco)[colnames(ptc_df_onco) == germline_col] <- "User_Classification"
 
 #### Check and remove any duplicated row ####
 ptc_df_onco[duplicated(ptc_df_onco),]
@@ -24,7 +52,7 @@ ptc_df_onco <- ptc_df_onco[!duplicated(ptc_df_onco),]
 
 table(ptc_df_onco$Phenotype)
 table(ptc_df_onco$Type)
-table(ptc_df_onco$Germline_Class)
+table(ptc_df_onco$User_Classification)
 
 #ptc_df_onco$Phenotype <- ifelse(ptc_df_onco$Phenotype == "Normal","N","T")
 
@@ -32,8 +60,8 @@ table(ptc_df_onco$Germline_Class)
 ptc_df_onco$Type <- toupper(substr(ptc_df_onco$Type, 1, 3))
 
 ### Abbreviate Germline Class values 
-ptc_df_onco$Germline_Class <-
-  sapply(ptc_df_onco$Germline_Class, function(x){
+ptc_df_onco$User_Classification <-
+  sapply(ptc_df_onco$User_Classification, function(x){
     if(x == "Likely benign"){
       y <- "LB"
     }else if(x == "Benign"){
@@ -54,7 +82,7 @@ ptc_df_onco$Genes <- sapply(ptc_df_onco$Genes, function(x) strsplit(x, ",")[[1]]
 ### Tabular view with value count of modified columns 
 table(ptc_df_onco$Phenotype)
 table(ptc_df_onco$Type)
-table(ptc_df_onco$Germline_Class)
+table(ptc_df_onco$User_Classification)
 
 ### Dimensions of subset dataset
 dim(ptc_df_onco)
@@ -73,21 +101,30 @@ write.csv(ptc_df_onco, file_name)
 
 ptc_df_ditto <- read.csv("data/PTC_DITTO.csv")
 colnames(ptc_df_ditto)
-sel_cols_onco_ditto <- c("Participant_id",
-                         "Phenotype",
-                         "Type",
-                         "Genes",
-                         "Variant",
-                         "Germline_Class",
-                         "Allelic.balance",
-                         "chrom",
-                         "pos",  
-                         "ref_base",
-                         "alt_base",
-                         "DITTO"
-)
+ditto_colnames <- colnames(ptc_df_ditto)
+ditto_colnames_clean <- normalize_colnames(ditto_colnames)
 
-ptc_df_onco_ditto <- ptc_df_ditto[,sel_cols_onco_ditto]
+resolve_ditto_col <- function(normalized_candidates) {
+  match_idx <- which(ditto_colnames_clean %in% normalized_candidates)
+  if (length(match_idx) == 0) {
+    return(NA_character_)
+  }
+  ditto_colnames[match_idx[1]]
+}
+
+ditto_participant_col <- resolve_ditto_col(c("participantid", "caseid"))
+ditto_variant_col <- resolve_ditto_col(c("variant"))
+ditto_score_col <- resolve_ditto_col(c("ditto"))
+
+if (anyNA(c(ditto_participant_col, ditto_variant_col, ditto_score_col))) {
+  stop(
+    "Required join columns were not found in DITTO table. ",
+    "Expected participant/case ID, variant, and DITTO score columns."
+  )
+}
+
+ptc_df_onco_ditto <- ptc_df_ditto[, c(ditto_participant_col, ditto_variant_col, ditto_score_col)]
+colnames(ptc_df_onco_ditto) <- c("Participant_id", "Variant", "DITTO")
 ptc_df_onco_ditto[duplicated(ptc_df_onco_ditto),]
 ptc_df_onco_ditto <- ptc_df_onco_ditto[!duplicated(ptc_df_onco_ditto),]
 ptc_ditto_only_scores <- ptc_df_onco_ditto[,c("Participant_id",
@@ -97,26 +134,26 @@ ptc_ditto_only_scores <- ptc_df_onco_ditto[,c("Participant_id",
 
 ptc_df_variants <- ptc_df_onco
 
-ptc_df_variants$DITTO <- sapply(1:nrow(ptc_df_onco), function(x){
+ptc_df_variants$DITTO <- sapply(seq_len(nrow(ptc_df_onco)), function(x){
   
-  participant_temp_id <- ptc_df_onco$Participant_id[x]
+  case_temp_id <- ptc_df_onco$Case_id[x]
   variant_temp_id <- ptc_df_onco$Variant[x]
   
-  ditto_score <- ptc_ditto_only_scores$DITTO[ptc_ditto_only_scores$Participant_id == participant_temp_id & ptc_ditto_only_scores$Variant == variant_temp_id]
+  ditto_score <- ptc_ditto_only_scores$DITTO[ptc_ditto_only_scores$Participant_id == case_temp_id & ptc_ditto_only_scores$Variant == variant_temp_id]
   
   ditto_score <- ifelse(is.null(ditto_score), NA, ditto_score)
-  print(paste(x,participant_temp_id, variant_temp_id, ditto_score, sep = "_"))
+  print(paste(x,case_temp_id, variant_temp_id, ditto_score, sep = "_"))
   return(ditto_score)
   
 }, USE.NAMES = FALSE)
 
 
 colnames(ptc_df_variants)
-colnames(ptc_df_variants) <- c("Participant ID","Phenotype",
-                                 "Variant Type", "Gene", 
-                                 "Variant", "Germline Class",
-                                 "Allelic Balance", "Chromosome",
-                                 "Position", "DITTO Score")
+colnames(ptc_df_variants) <- c("Case_id","Phenotype",
+                               "Variant Type", "Gene", 
+                               "Variant", "User_Classification",
+                               "Allelic Balance", "Chromosome",
+                               "Position", "DITTO Score")
 ptc_df_variants[,"DITTO Score"] <- round(ptc_df_variants[,"DITTO Score"], 4)
 
 rm(ptc_ditto_only_scores)
